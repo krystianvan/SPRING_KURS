2631394fccd7fcf15d9049c523dc91d3d6895e7f
krystianvan <65135056+krystianvan@users.noreply.github.com>
2021-05-10 21:03:26 +0200

    Sekcja_4

---
+++ todo-app/todo-app/src/main/java/io/github/mat3e/model/Task.java
@@ -0,0 +1,46 @@
+package io.github.mat3e.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @NotBlank(message = "Task's description must not be empty")
+    private String description;
+    private boolean done;
+
+    Task() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    void setDone(final boolean done) {
+        this.done = done;
+    }
+}
